58a470f9ce86f7886671073e3feab262e71bc5ca
Mohamed Camara <mohamed.camara@lilo.org>
2020-05-16 13:57:31 +0200

    utilisation de react router V4 et gestion de page introuvable

---
+++ src/components/notFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound = () => {
+   return <h2 className = 'notFound'> Page introuvable </h2>
+}
+
+export default NotFound
--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Connexion from './components/Connexion'
+import notFound from './components/notFound'
 import * as serviceWorker from './serviceWorker'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import NotFound from './components/notFound'
 
-ReactDOM.render(<Connexion />, document.getElementById('root'))
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <Switch>        {/* Switch permet de prendre plusieur root (URL)*/}       
+                <Route exact path ='/' component= {Connexion} />
+                <Route path ='/pseudo/:pseudo'  component = {App}/>
+                    {/* 
+                    path = '/' signifie quand on est à la racine
+                    le mot clé "exact" signifie qu'on ne doit avoir que la racine '/' et rien d'autre après
+                    
+                    path= '/pseudo/:unePage' signifie qu'on ne sait pas ce qui arrive après pseudo
+                    */}
+                <Route component = {NotFound} />
+                {/* pas besoin de mettre le path car il exécute ce code 
+                quand le chemin n'est pas trouvé */}
+            
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
