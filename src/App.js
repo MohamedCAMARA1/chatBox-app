9419100eb52c475a47f0adf07a04878eec51f41e
Mohamed Camara <mohamed.camara@lilo.org>
2020-05-18 12:30:56 +0200

    gestion des messages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
 import Formulaire from './components/Formulaire'
+import Message from './components/Message'
 
 class App extends Component {
+
+  state = {
+    messages : {},
+    pseudo : this.props.match.params.pseudo //grace au routeur, on peut acceder à plusieur paramètres
+                                            //nottemment celui de l'url
+  }
+
+  addMessage = message => {
+    const messages = {...this.state.messages}
+    messages[`message-${Date.now()}`] = message //permet d'avoir un format message-dateDuMoment
+                                           //ce qui permet également d'avoir 1 unicité des messages
+                                              
+    this.setState({ messages })
+  }
+
+
   render () {
     return (
       <div className='box'>
         <div>
           <div className = 'message'>  
-              message
+              <Message/>
+              <Message/>
           </div>
         </div>
 
-        <Formulaire />
+        <Formulaire 
+        pseudo = {this.state.pseudo}
+        addMessage={this.addMessage}
+        />
 
       </div>
     )
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Message =()=> {
+   
+        return (
+            <p className='user-message'>                
+                le message
+            </p>
+        );    
+}
+
+export default Message;
